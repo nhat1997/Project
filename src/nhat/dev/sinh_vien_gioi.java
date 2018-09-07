781e85321b7554805a14e1d9609713e7721ba1db
nhat <nhat1997.com>
2018-09-07 16:01:50 +0700

    dv_sinh_vien

---
+++ src/nhat/dev/sinh_vien_gioi.java
@@ -0,0 +1,25 @@
+package nhat.dev;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class sinh_vien_gioi {
+
+	public static void main (String[]args) {
+		try {
+			FileInputStream fis = new FileInputStream("in.txt");
+
+			BufferedReader br = new BufferedReader (new InputStreamReader(fis));
+			
+			String line ;
+			
+			while ((line = br.readLine()) !=null) {
+				System.out.println(line);
+			}
+			br.close ();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}		
+	}
+}
